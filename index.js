0c66ed281de3abb848f80884141317e8d0787bfa
wangchuan <wangchuan@tongtusoft.com.cn>
2022-04-29 17:49:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const DependencyCheckPlugin = require('./lib/dependency-check-plugin.js');
+
+module.exports = {
+  DependencyCheckPlugin,
+};
---
+++ test/src/main.js
@@ -0,0 +1,2 @@
+import { merge } from 'lodash-es';
+import dayjs from 'dayjs';
